22e4c141f675dfdb3ab6e9bacdc1d6c0e0ed93be
presh-031 <akingbulugbe.dev@gmail.com>
2022-09-18 23:23:10 +0100

    dom elements working, onto styling and then adding filtering functionality

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 getAllCountriesOnLoad();
 function getAllCountriesOnLoad() {
-  // const url = "https://restcountries.com/v3.1/all";
+  const url = "https://restcountries.com/v3.1/all";
   fetch(url)
     .then((res) => res.json()) //parse response as JSON
     .then((data) => {
@@ -17,8 +17,7 @@ function getAllCountriesOnLoad() {
 
 function updateDOMOnPageLoad(data) {
   const gridContainer = document.querySelector(".countries-container");
-  const countryInfo = document.querySelector(".country-info");
-  // console.log(data);
+  console.log(data);
   data.forEach((country) => {
     // create-elements
     const eachCountry = document.createElement("div");
@@ -26,11 +25,17 @@ function updateDOMOnPageLoad(data) {
     const flagImage = document.createElement("img");
     const infoContainer = document.createElement("div");
     const countryName = document.createElement("h3");
+    const countryInfoPop = document.createElement("p");
+    const countryInfoReg = document.createElement("p");
+    const countryInfoCap = document.createElement("p");
+    const boldPop = document.createElement("span");
+    const boldReg = document.createElement("span");
+    const boldCap = document.createElement("span");
     const countryPopulation = document.createElement("span");
     const countryRegion = document.createElement("span");
     const countryCapital = document.createElement("span");
-    console.log(country.flags.png);
-
+    // console.log(country.flags.png);
+    console.log(eachCountry);
     // add classes
     gridContainer.classList.add("countries-container");
     eachCountry.classList.add("each-country");
@@ -38,7 +43,12 @@ function updateDOMOnPageLoad(data) {
     flagImage.classList.add("flag-image");
     infoContainer.classList.add("info-container");
     countryName.classList.add("country-name");
-    countryInfo.classList.add("country-info");
+    countryInfoPop.classList.add("country-info-pop");
+    countryInfoReg.classList.add("country-info-reg");
+    countryInfoCap.classList.add("country-info-cap");
+    boldPop.classList.add("bold-pop");
+    boldReg.classList.add("bold-reg");
+    boldCap.classList.add("bold-cap");
     countryPopulation.classList.add("country-population");
     countryRegion.classList.add("country-region");
     countryCapital.classList.add("country-capital");
@@ -49,6 +59,30 @@ function updateDOMOnPageLoad(data) {
     flagContainer.append(flagImage);
     eachCountry.append(infoContainer);
     infoContainer.append(countryName);
-    infoContainer.append(countryInfo);
+    infoContainer.append(countryInfoPop);
+    infoContainer.append(countryInfoReg);
+    infoContainer.append(countryInfoCap);
+    countryInfoPop.append(boldPop);
+    countryInfoPop.append(countryPopulation);
+    countryInfoReg.append(boldReg);
+    countryInfoReg.append(countryRegion);
+    countryInfoCap.append(boldCap);
+    countryInfoCap.append(countryCapital);
+
+    // update content as appropriate
+
+    console.log(country.name.common);
+    console.log(country.population);
+    console.log(country.capital[0]);
+    console.log(country.region);
+
+    flagImage.src = country.flags.svg;
+    boldPop.innerHTML = "Population: ";
+    boldReg.innerHTML = "Region: ";
+    boldCap.innerHTML = "Capital: ";
+    countryName.innerHTML = country.name.common;
+    countryPopulation.innerHTML = country.population;
+    countryRegion.innerHTML = country.region;
+    countryCapital.innerHTML = country.capital[0];
   });
 }
