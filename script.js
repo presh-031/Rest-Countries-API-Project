55d80252b277eb5918180caf221e3b63cdd5b79d
presh-031 <akingbulugbe.dev@gmail.com>
2022-09-18 17:47:26 +0100

    still working updating dom with fetched data

--- script.js
+++ script.js
@@ -2,15 +2,23 @@
 
 getAllCountriesOnLoad();
 function getAllCountriesOnLoad() {
-  const url = "https://restcountries.com/v3.1/all";
+  // const url = "https://restcountries.com/v3.1/all";
   fetch(url)
     .then((res) => res.json()) //parse response as JSON
     .then((data) => {
       // console.log(data);
-      console.log(data.length);
-      console.log(data.forEach(country => {});
+      // console.log(data.length);
+      updateDOMOnPageLoad(data);
     })
     .catch((err) => {
       console.log(`error ${err}`);
     });
+}
+
+function updateDOMOnPageLoad(data) {
+  const gridContainer = document.querySelector(".countries-container");
+  // console.log(data);
+  data.forEach((country) => {
+    const gridItem = document.createElement("div");
+  });
 }
